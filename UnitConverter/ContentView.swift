aa7723e5f8272afb959533ee639eb9ea0a0e204c
Srujan Sannidhi <srujan.sannidhi7@gmail.com>
2024-12-29 12:31:58 -0600

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Srujan Sannidhi on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitConverterTests/UnitConverterTests.swift
@@ -0,0 +1,16 @@
+//
+//  UnitConverterTests.swift
+//  UnitConverterTests
+//
+//  Created by Srujan Sannidhi on 12/29/24.
+//
+
+import Testing
+
+struct UnitConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
